a176e7c475cb82610f745f5cf3789b192facacec
Christopher Marzulli <chriszulli15@gmail.com>
2022-09-01 14:42:19 -0400

    commit tree 4e03316bfd3bb7c7af952da4adbef1638f61492b

--- config/environment.rb
+++ config/environment.rb
@@ -10,3 +10,14 @@ ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: "./db/test.sqlite3"
 )
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+  )
+  SQL
+
+  ActiveRecord::Base.connection.execute(sql)
+  
